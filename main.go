7837b174c66e0d9e647bcf0902939abe109042d5
Mohi Beyki <mohibeyki@gmail.com>
2021-02-05 19:37:52 -0500

    changed starting at to running at

--- main.go
+++ main.go
@@ -19,6 +19,6 @@ func main() {
 	rootRouter := router.Init(database.GetDB())
 
 	socketString := fmt.Sprintf("%s:%d", config.Server.Host, config.Server.Port)
-	log.Printf("Server is starting at %s", socketString)
+	log.Printf("Server is running at %s", socketString)
 	rootRouter.Run(socketString)
 }
